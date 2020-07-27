c78652763f4ec72ab1dfc67f6937af985f69d4e2
popanoas <60436785+popanoas@users.noreply.github.com>
2020-07-27 14:37:01 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -3,11 +3,16 @@ import discord
 import asyncio
 import os
 
-ID_CHANNEL_1 = 673477447785644042  # 1チャンネルID（事前設定用）
-ID_CHANNEL_2 = 673477457550114826  # 2チャンネルID（事前設定用）
-ID_CHANNEL_3 = 673477466274398208  # 3チャンネルID（事前設定用）
-ID_CHANNEL_4 = 673477474293645313  # 4チャンネルID（事前設定用）
-ID_CHANNEL_5 = None  # 5チャンネルID（事前設定用）
+ID_CHANNEL_1 = 670294227846037514  # 1チャンネルID（事前設定用）
+ID_CHANNEL_2 = 715596202032496760  # 2チャンネルID（事前設定用）
+ID_CHANNEL_3 = 670294262696509469  # 3チャンネルID（事前設定用）
+ID_CHANNEL_4 = 715596251927675023  # 4チャンネルID（事前設定用）
+ID_CHANNEL_5 = 715596307762380811  # 5チャンネルID（事前設定用）
+ID_CHANNEL_6 = 715596307762380811  # 6チャンネルID（事前設定用）
+ID_CHANNEL_7 = 670294326131032064  # 7チャンネルID（事前設定用）
+ID_CHANNEL_8 = 715596648570552341  # 8チャンネルID（事前設定用）
+ID_CHANNEL_9 = 670294357944958976  # 9チャンネルID（事前設定用）
+ID_CHANNEL_10 = 715596743450034188  # 10チャンネルID（事前設定用）
 token = os.environ['DISCORD_BOT_TOKEN']
 
 # 接続に必要なオブジェクトを生成
@@ -19,24 +24,24 @@ async def on_message(message):
     # メッセージ送信者がBotだった場合は無視する
     if message.author.bot:
         return
-    # 「/1段階目」と発言したら「一段階目」が返る処理
+ # チャンネル1に対するアクション
     if message.content == '/1段階目':
         channel = client.get_channel(ID_CHANNEL_1)
         await channel.send('--------------------1段階目--------------------')
-    # 「/2段階目」と発言したら「一段階目」が返る処理
+        
     if message.content == '/2段階目':
         channel = client.get_channel(ID_CHANNEL_1)
         await channel.send('--------------------2段階目--------------------')
-    # 「/3段階目」と発言したら「一段階目」が返る処理
+  
     if message.content == '/3段階目':
         channel = client.get_channel(ID_CHANNEL_1)
         await channel.send('--------------------3段階目--------------------')
-    # 「/4段階目」と発言したら「一段階目」が返る処理
+
     if message.content == '/4段階目':
         channel = client.get_channel(ID_CHANNEL_1)
         await channel.send('--------------------4段階目--------------------')
         
-     # 「/1段階目」と発言したら「一段階目」が返る処理
+ # チャンネル2に対するアクション
     if message.content == '/1段階目':
         channel = client.get_channel(ID_CHANNEL_2)
         await channel.send('--------------------1段階目--------------------')
@@ -46,11 +51,148 @@ async def on_message(message):
         await channel.send('--------------------2段階目--------------------')
     # 「/3段階目」と発言したら「一段階目」が返る処理
     if message.content == '/3段階目':
-        channel = client.get_channel(ID_CHANNEL_3)
+        channel = client.get_channel(ID_CHANNEL_2)
         await channel.send('--------------------3段階目--------------------')
     # 「/4段階目」と発言したら「一段階目」が返る処理
     if message.content == '/4段階目':
-        channel = client.get_channel(ID_CHANNEL_4)
+        channel = client.get_channel(ID_CHANNEL_2)
+        await channel.send('--------------------4段階目--------------------')      
+        
+ # チャンネル3に対するアクション
+    if message.content == '/1段階目':
+        channel = client.get_channel(ID_CHANNEL_3)
+        await channel.send('--------------------1段階目--------------------')
+
+    if message.content == '/2段階目':
+        channel = client.get_channel(ID_CHANNEL_3)
+        await channel.send('--------------------2段階目--------------------')
+
+    if message.content == '/3段階目':
+        channel = client.get_channel(ID_CHANNEL_3)
+        await channel.send('--------------------3段階目--------------------')
+
+    if message.content == '/4段階目':
+        channel = client.get_channel(ID_CHANNEL_3)
         await channel.send('--------------------4段階目--------------------')
+        
+ # チャンネル4に対するアクション
+    if message.content == '/1段階目':
+        channel = client.get_channel(ID_CHANNEL_4)
+        await channel.send('--------------------1段階目--------------------')
+
+    if message.content == '/2段階目':
+        channel = client.get_channel(ID_CHANNEL_4)
+        await channel.send('--------------------2段階目--------------------')
+
+    if message.content == '/3段階目':
+        channel = client.get_channel(ID_CHANNEL_4)
+        await channel.send('--------------------3段階目--------------------')
+
+    if message.content == '/4段階目':
+        channel = client.get_channel(ID_CHANNEL_4)
+        await channel.send('--------------------4段階目--------------------')   
+        
+ # チャンネル5に対するアクション
+    if message.content == '/1段階目':
+        channel = client.get_channel(ID_CHANNEL_5)
+        await channel.send('--------------------1段階目--------------------')
+
+    if message.content == '/2段階目':
+        channel = client.get_channel(ID_CHANNEL_5)
+        await channel.send('--------------------2段階目--------------------')
+
+    if message.content == '/3段階目':
+        channel = client.get_channel(ID_CHANNEL_5)
+        await channel.send('--------------------3段階目--------------------')
+
+    if message.content == '/4段階目':
+        channel = client.get_channel(ID_CHANNEL_5)
+        await channel.send('--------------------4段階目--------------------')   
+ 
+ # チャンネル6に対するアクション
+    if message.content == '/1段階目':
+        channel = client.get_channel(ID_CHANNEL_6)
+        await channel.send('--------------------1段階目--------------------')
+
+    if message.content == '/2段階目':
+        channel = client.get_channel(ID_CHANNEL_6)
+        await channel.send('--------------------2段階目--------------------')
+
+    if message.content == '/3段階目':
+        channel = client.get_channel(ID_CHANNEL_6)
+        await channel.send('--------------------3段階目--------------------')
+
+    if message.content == '/4段階目':
+        channel = client.get_channel(ID_CHANNEL_6)
+        await channel.send('--------------------4段階目--------------------')   
+
+ # チャンネル7に対するアクション
+    if message.content == '/1段階目':
+        channel = client.get_channel(ID_CHANNEL_7)
+        await channel.send('--------------------1段階目--------------------')
+
+    if message.content == '/2段階目':
+        channel = client.get_channel(ID_CHANNEL_7)
+        await channel.send('--------------------2段階目--------------------')
+
+    if message.content == '/3段階目':
+        channel = client.get_channel(ID_CHANNEL_7)
+        await channel.send('--------------------3段階目--------------------')
+
+    if message.content == '/4段階目':
+        channel = client.get_channel(ID_CHANNEL_7)
+        await channel.send('--------------------4段階目--------------------')          
+        
+ # チャンネル8に対するアクション
+    if message.content == '/1段階目':
+        channel = client.get_channel(ID_CHANNEL_8)
+        await channel.send('--------------------1段階目--------------------')
+
+    if message.content == '/2段階目':
+        channel = client.get_channel(ID_CHANNEL_8)
+        await channel.send('--------------------2段階目--------------------')
+
+    if message.content == '/3段階目':
+        channel = client.get_channel(ID_CHANNEL_8)
+        await channel.send('--------------------3段階目--------------------')
+
+    if message.content == '/4段階目':
+        channel = client.get_channel(ID_CHANNEL_8)
+        await channel.send('--------------------4段階目--------------------')          
+        
+ # チャンネル9に対するアクション
+    if message.content == '/1段階目':
+        channel = client.get_channel(ID_CHANNEL_9)
+        await channel.send('--------------------1段階目--------------------')
+
+    if message.content == '/2段階目':
+        channel = client.get_channel(ID_CHANNEL_9)
+        await channel.send('--------------------2段階目--------------------')
+
+    if message.content == '/3段階目':
+        channel = client.get_channel(ID_CHANNEL_9)
+        await channel.send('--------------------3段階目--------------------')
+
+    if message.content == '/4段階目':
+        channel = client.get_channel(ID_CHANNEL_9)
+        await channel.send('--------------------4段階目--------------------')          
+ 
+ # チャンネル10に対するアクション
+    if message.content == '/1段階目':
+        channel = client.get_channel(ID_CHANNEL_10)
+        await channel.send('--------------------1段階目--------------------')
+
+    if message.content == '/2段階目':
+        channel = client.get_channel(ID_CHANNEL_10)
+        await channel.send('--------------------2段階目--------------------')
+
+    if message.content == '/3段階目':
+        channel = client.get_channel(ID_CHANNEL_10)
+        await channel.send('--------------------3段階目--------------------')
+
+    if message.content == '/4段階目':
+        channel = client.get_channel(ID_CHANNEL_10)
+        await channel.send('--------------------4段階目--------------------')  
+        
 # Botの起動とDiscordサーバーへの接続
 client.run(token)
