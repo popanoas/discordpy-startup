468cc68a42c128b971d393cc411c07e7d65b4320
popanoas <60436785+popanoas@users.noreply.github.com>
2020-08-03 07:55:32 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -266,7 +266,7 @@ async def on_message(message):
 async def loop():
     # 現在の時刻
     now = datetime.now().strftime('%H:%M')
-    if now == '22:10':
+    if now == '22:56':
         channel = client.get_channel(CHANNEL_ID)
         await channel.send('おはるる～')  
 #ループ処理実行
