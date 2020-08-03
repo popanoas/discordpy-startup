19031cc0e94db6db36d7c90836ec7d6d0efafd0f
PoppyMania <52383716+PoppyMania@users.noreply.github.com>
2020-08-03 16:11:10 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -261,10 +261,7 @@ async def on_raw_reaction_add(payload):
         guild = client.get_guild(payload.guild_id)  
         member = guild.get_member(payload.user_id)    
         user = client.get_user(payload.message_id)
-        if user.bot:
-            return
-        else:
-            await channel.send(member.name + 'さんは今日のサポートを借り終えました')  
+        await channel.send(member.name + 'さんは今日のサポートを借り終えました')  
 
 # 60秒に一回ループ
 @tasks.loop(seconds=60)
