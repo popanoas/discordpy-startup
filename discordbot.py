d6f7b8029f1a19d9642699115951c64d7bd5e2fd
popanoas <60436785+popanoas@users.noreply.github.com>
2020-10-18 13:52:23 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -11,7 +11,7 @@ import threading
 #鯖チャンネルID
 #ID_CHANNEL_1 = 670294227846037514
 ID_CHANNEL_ZANGE = 741739653245173800
-ID_taskkill = 731046340674453567
+ID_taskkill = 726398497384824853
 ID_Mana = 730136347477540908
 #ロールID
 ID_role_1 = 767199879641694268
@@ -25,7 +25,6 @@ ID_emoji_zange = '<:61ok:728923368870510605>'
 ID_1 = '<:59na:726842370116812850>'
 ID_2 = '<:58no:726842380673876091>'
 ID_3 = '<:57ra:726842390949789696>'
-ID_reset = '<:knp:758012336706683062>'
 token = os.environ['DISCORD_BOT_TOKEN']
 # 接続に必要なオブジェクトを生成
 client = discord.Client()
@@ -63,8 +62,7 @@ async def loop():
         
         msg = await channel.send('今日のタスクキル')
         await msg.add_reaction(ID_tk)
-              
-        
+
         role = discord.utils.get(message.guild.roles, name='1凸')
         await role.delete()
         role = discord.utils.get(message.guild.roles, name='2凸')
@@ -72,12 +70,10 @@ async def loop():
         role = discord.utils.get(message.guild.roles, name='3凸')
         await role.delete() 
         role = discord.utils.get(message.guild.roles, name='タスクキル済')
-        await role.delete()
-          
-        
-#ループ処理実行
-loop.start()  
+        await role.delete()        
 
+#ループ処理実行
+loop.start()    
 #@client.event
 #async def on_raw_reaction_add(payload):
     # author: リアクションがついたメッセージを書いた人
@@ -112,12 +108,12 @@ async def on_reaction_add(self, reaction, user):
 async def on_raw_reaction_add(payload):  
     channel = client.get_channel(payload.channel_id)  
     if channel.id == ID_taskkill:
+        #if payload.emoji.name == '\N{GRINNING FACE}':
         if str(payload.emoji) == '<:59na:726842370116812850>':
             guild = client.get_guild(payload.guild_id)  
             member = guild.get_member(payload.user_id)  
             role = guild.get_role(ID_role_1)  
             await member.add_roles(role)  
-
         if str(payload.emoji) == '<:58no:726842380673876091>':
             guild = client.get_guild(payload.guild_id)  
             member = guild.get_member(payload.user_id)  
@@ -135,7 +131,6 @@ async def on_raw_reaction_add(payload):
             member = guild.get_member(payload.user_id)  
             role = guild.get_role(ID_role_tk)  
             await member.add_roles(role)  
-
             
 @client.event
 async def on_message(message):
@@ -148,11 +143,11 @@ async def on_message(message):
         role = discord.utils.get(message.guild.roles, name='1凸')
         await role.delete()
         role = discord.utils.get(message.guild.roles, name='2凸')
-        await role.delete()            
+        await role.delete()
         role = discord.utils.get(message.guild.roles, name='3凸')
-        await role.delete()            
+        await role.delete()        
         role = discord.utils.get(message.guild.roles, name='タスクキル済')
-        await role.delete()   
-        await message.delete()            
-            
+        await role.delete()        
+        
+        
 client.run(token)
