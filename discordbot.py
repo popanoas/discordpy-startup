4adca5c7679ab215d0896c22435232623a72f71e
popanoas <60436785+popanoas@users.noreply.github.com>
2020-10-18 13:37:42 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -25,6 +25,7 @@ ID_emoji_zange = '<:61ok:728923368870510605>'
 ID_1 = '<:59na:726842370116812850>'
 ID_2 = '<:58no:726842380673876091>'
 ID_3 = '<:57ra:726842390949789696>'
+ID_reset = :x:
 token = os.environ['DISCORD_BOT_TOKEN']
 # 接続に必要なオブジェクトを生成
 client = discord.Client()
@@ -70,7 +71,10 @@ async def loop():
         role = discord.utils.get(message.guild.roles, name='3凸')
         await role.delete() 
         role = discord.utils.get(message.guild.roles, name='タスクキル済')
-        await role.delete()        
+        await role.delete()
+        
+        msg = await channel.send('リセット')
+        await msg.add_reaction(ID_reset)        
         
 #ループ処理実行
 loop.start()    
@@ -108,7 +112,6 @@ async def on_reaction_add(self, reaction, user):
 async def on_raw_reaction_add(payload):  
     channel = client.get_channel(payload.channel_id)  
     if channel.id == ID_taskkill:
-        #if payload.emoji.name == '\N{GRINNING FACE}':
         if str(payload.emoji) == '<:59na:726842370116812850>':
             guild = client.get_guild(payload.guild_id)  
             member = guild.get_member(payload.user_id)  
@@ -132,6 +135,16 @@ async def on_raw_reaction_add(payload):
             member = guild.get_member(payload.user_id)  
             role = guild.get_role(ID_role_tk)  
             await member.add_roles(role)  
+
+        if str(payload.emoji) == ':x:':      
+        　　　　role = discord.utils.get(message.guild.roles, name='1凸')
+        　　　　await role.delete()
+        　　　　role = discord.utils.get(message.guild.roles, name='2凸')
+        　　　　await role.delete()            
+        　　　　role = discord.utils.get(message.guild.roles, name='3凸')
+        　　　　await role.delete()            
+        　　　　role = discord.utils.get(message.guild.roles, name='タスクキル済')
+        　　　　await role.delete()       
             
 @client.event
 async def on_message(message):
@@ -141,7 +154,13 @@ async def on_message(message):
         
     # チャンネル1に対するアクション
     if message.content == '/ロールを外す':
-        role = discord.utils.get(message.guild.roles, name='1凸')
-        await role.delete()
+        　　　　role = discord.utils.get(message.guild.roles, name='1凸')
+        　　　　await role.delete()
+        　　　　role = discord.utils.get(message.guild.roles, name='2凸')
+        　　　　await role.delete()            
+        　　　　role = discord.utils.get(message.guild.roles, name='3凸')
+        　　　　await role.delete()            
+        　　　　role = discord.utils.get(message.guild.roles, name='タスクキル済')
+        　　　　await role.delete()   
             
 client.run(token)
