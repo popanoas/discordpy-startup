a36512b3af1a17d029c0131c0cc0b9e96c703695
popanoas <60436785+popanoas@users.noreply.github.com>
2020-10-18 13:05:52 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -100,7 +100,7 @@ async def on_raw_reaction_add(payload):
     channel = client.get_channel(payload.channel_id)  
     if channel.id == ID_taskkill:
         #if payload.emoji.name == '\N{GRINNING FACE}':
-        if str(payload.emoji) == 'ID_1':
+        if str(payload.emoji) == '<:59na:726842370116812850>':
             guild = client.get_guild(payload.guild_id)  
             member = guild.get_member(payload.user_id)  
             role = guild.get_role(ID_role_1)  
