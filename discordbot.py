2ca5c31b23ccff8f7083af59faa7d3a935a12e12
popanoas <60436785+popanoas@users.noreply.github.com>
2020-10-18 11:44:14 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -25,9 +25,9 @@ ID_role_tk = 767200196827676683
 ID_emoji = '<:61ok:728923368870510605>'
 ID_tk = '<:syarururage:737890640519495712>'
 ID_emoji_zange = '<:61ok:728923368870510605>'
-ID_1 = '<:59na:726842370116812850>'
-ID_2 = '<:58no:726842380673876091>'
-ID_3 = '<:57ra:726842390949789696>'
+ID_emoji_1 = '<:59na:726842370116812850>'
+ID_emoji_2 = '<:58no:726842380673876091>'
+ID_emoji_3 = '<:57ra:726842390949789696>'
 
 token = os.environ['DISCORD_BOT_TOKEN']
 
@@ -105,15 +105,28 @@ async def on_reaction_add(self, reaction, user):
             text = "父と子とゴデチアのみ名によって、" + message.author.name + "の罪をゆるします。アーメン。安心して行きなさい"
     await channel.send(text)
 
-@client.event  
-async def on_raw_reaction_add(payload):  
-    channel = client.get_channel(payload.channel_id)  
-    if channel.id == ID_taskkill:
-        if payload.emoji.name == '<:59na:726842370116812850>':
-            guild = client.get_guild(payload.guild_id)  
-            member = guild.get_member(payload.user_id)  
-            role = guild.get_role(ID_role_1)  
-            await member.add_roles(role)  
+# 絵文字で役職付与
+async def grant_role(payload):
+    # 絵文字が異なる場合は処理を打ち切る
+    if payload.emoji.name != ID_emoji_1: 
+        return
 
+    # チャンネルが異なる場合は処理を打ち切る
+    if payload.channel_id != ID_taskkill:
+        return
+
+    # Member オブジェクトと Role オブジェクトを取得して役職を付与
+    member = payload.member
+    role = guild.get_role(ID_role_1)
+    await member.add_roles(role)
+    return member
+
+# リアクション追加時に実行されるイベントハンドラを定義
+@client.event
+async def on_raw_reaction_add(payload):
+    member = await grant_role(payload)
+    if member is not None: # 役職を付与したメンバーがいる時
+        text = f'{member.mention} おつかれさま！'
+        await client.get_channel(ID_taskkill).send(text)
             
 client.run(token)
