b5df3b7f767610487ffc2aa926814f049f289a05
popanoas <60436785+popanoas@users.noreply.github.com>
2020-10-19 10:59:56 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -47,7 +47,7 @@ async def on_message(message):
 async def loop():
     # 現在の時刻
     now = datetime.now().strftime('%H:%M')
-    if now == '10:56':
+    if now == '05:00':
         #ランドソル杯データ入力
         #channel = client.get_channel(ID_Mana)
         #msg = await channel.send('日付が変わりました！記入が終わったらリアクションを付けてね♡ \n https://docs.google.com/spreadsheets/d/1nCdtFHS-60WcRZDx8hTXHFm3mPuEqefntQxeRfM2Lv0/edit#gid=632518118')  
@@ -71,7 +71,7 @@ async def loop():
         
     if message.author.bot:
         return
-    if now == '10:56':    
+    if now == '05:00':    
         role1 = guild.get_role(ID_role_1)
         role2 = guild.get_role(ID_role_2)
         role3 = guild.get_role(ID_role_3)
