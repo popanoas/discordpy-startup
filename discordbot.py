aec81714b5b420f2599db658697b3b0f215c358f
popanoas <60436785+popanoas@users.noreply.github.com>
2020-10-21 17:02:14 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -118,14 +118,13 @@ async def on_raw_reaction_add(payload):
 @client.event  
 async def on_raw_reaction_remove(payload):  
     channel = client.get_channel(payload.channel_id)
-        #readmeでの役職の付与
     if channel.id == ID_readme:
         if str(payload.emoji) == '<:61ok:728923368870510605>':
             guild = client.get_guild(payload.guild_id)  
             member = guild.get_member(payload.user_id)  
-            role = guild.get_role(ID_clanmember)
+            role = guild.get_role(ID_role_1)
             if not member.bot:            
-                await member.remove_roles(role)   
+                await member.remove_roles(role)  
                 
     if channel.id == ID_taskkill:
         #ロールの付与
