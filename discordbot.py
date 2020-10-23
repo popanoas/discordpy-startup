8fa863b0ab2188e99ddf98b704a794803fa3bd66
popanoas <60436785+popanoas@users.noreply.github.com>
2020-10-23 09:23:31 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -7,6 +7,7 @@ import random
 from datetime import datetime
 from discord.ext import tasks
 from discord.ext import commands
+from googlesearch import search
 import threading
 #鯖チャンネルID
 #ID_CHANNEL_1 = 670294227846037514
@@ -232,7 +233,23 @@ async def on_message(message):
         await msg.add_reaction(ID_tk)
         msg = await channel.send('凸状況の初期化')        
         await msg.add_reaction(ID_remove_role)   
-        msg = await channel.send('----------------------------------------------------------------------')            
+        msg = await channel.send('----------------------------------------------------------------------')      
+        
+@client.event
+async def on_message(message):
+    if message.author.bot:
+        return
+   # google検索モード(次に何か入力されるとそれを検索)
+        kensaku = message.content
+        # 日本語で検索した上位1件を順番に表示
+        for url in search(kensaku, lang="jp",num = 1):
+            await message.channel.send(url)
+            count += 1
+            if(count == 1):
+               break
+    # google検索モードへの切り替え
+    if message.content == '!google':
+        await message.channel.send('検索するワードをチャットで発言してね')        
         
         
 client.run(token)
